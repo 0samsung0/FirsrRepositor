cc1697f5a6baa8b31858a401401db31385466548
0samsung0 <78357262+0samsung0@users.noreply.github.com>
2024-07-17 15:14:02 +0300

    add @ to field

--- src/main/java/com/example/practisewithoutsequrity/entity/Message.java
+++ src/main/java/com/example/practisewithoutsequrity/entity/Message.java
@@ -1,10 +1,20 @@
 package com.example.practisewithoutsequrity.entity;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@Table(name = "Task")
+@ToString
 public class Message {
     private Long id;
 
--- src/main/java/com/example/practisewithoutsequrity/entity/Status.java
+++ src/main/java/com/example/practisewithoutsequrity/entity/Status.java
@@ -1,16 +1,27 @@
 package com.example.practisewithoutsequrity.entity;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@Table(name = "Status")
+@ToString
 public class Status {
 
     private Long id;
     private Double statusTime;
     private Double statusTask;
-    
+
 
 
 
